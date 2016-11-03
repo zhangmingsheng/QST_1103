f57b2f0fb4c4c82f693a22b641e63d19d8ab7417
zhangmingsheng <z1m6s3@163.com>
2016-11-03 17:29:26 +0800

    Update MapperOfIPFilter.java

--- ex4/MapperOfIPFilter.java
+++ ex4/MapperOfIPFilter.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class MapperOfIPFilter {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
+		while (scanner.hasNext()) {
 			String line = scanner.nextLine();
+			String[] str = null;
+			str = line.split(" ");
+			String st = str[3].substring(1,str[3].length());
+			if (str.length > 6) {
+				System.out.println(str[0] + "\t"+st);
+			}
 		}
 	}
 }
