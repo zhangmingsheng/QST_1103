0653a9848a96eaab47cf10c8ca8868e48a509897
zhangmingsheng <z1m6s3@163.com>
2016-11-03 17:30:48 +0800

    Update ReducerOfIPFilter.java

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,36 @@
 package No4;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ReducerOfIPFilter {
-	public static void main(String[] args){
+	public static void main(String[] args) throws ParseException {
+		Locale locale = Locale.US;
+		SimpleDateFormat inputFormat = new SimpleDateFormat(
+				"dd/MMM/yyyy:HH:mm:ss", locale);
+		SimpleDateFormat regularFormat = new SimpleDateFormat(
+				"yyyy-MM-dd-HH:mm:ss");
+		HashSet<Object> hs = new HashSet<Object>();
 		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
+		String[] str = null;
+		Date beginDate = regularFormat.parse(args[0]);
+		Date endDate = regularFormat.parse(args[1]);
+		while (scanner.hasNext()) {
 			String line = scanner.nextLine();
+			str = line.split("\t");
+			Date lineDate = inputFormat.parse(str[1]);
+			if (lineDate.compareTo(endDate) < 0) {
+				if (lineDate.compareTo(beginDate) > 0) {
+					hs.add(str[0]);
+				}
+			}
 		}
+		System.out.println("Between " + args[0] + " and " + args[1]
+				+ " IP number: " + hs.size());
 	}
 }
+
