a61f8c5f7fc1f7b5a926c9ebe3a4a1e3a716a8bf
zhangmingsheng <z1m6s3@163.com>
2016-11-03 14:47:19 +0800

    Update DifferentFormat.java

--- ex2/DifferentFormat.java
+++ ex2/DifferentFormat.java
@@ -18,8 +18,8 @@ public class DifferentFormat {
 	@SuppressWarnings("resource")
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		Locale locale = Locale.US; 
-		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", locale); //修改格式
+		Locale locale = Locale.US; //定义语言
+		SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", locale); //修改格式
 		while (scanner.hasNext()){
 			String line = scanner.nextLine();
 			Date lineDate = null;
