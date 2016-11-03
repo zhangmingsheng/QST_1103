fa95a42e7c59b282ecfdc2f9396e42d3b9639a68
zhangmingsheng <z1m6s3@163.com>
2016-11-03 18:33:35 +0800

    Update MapperOfIPFilter.java

--- ex4/MapperOfIPFilter.java
+++ ex4/MapperOfIPFilter.java
@@ -8,7 +8,9 @@ public class MapperOfIPFilter {
 		while (scanner.hasNext()) {
 			String line = scanner.nextLine();
 			String[] str = null;
+			//用空格把输入数据分割开
 			str = line.split(" ");
+			//去掉时间前面的“[”
 			String st = str[3].substring(1,str[3].length());
 			if (str.length > 6) {
 				System.out.println(str[0] + "\t"+st);
