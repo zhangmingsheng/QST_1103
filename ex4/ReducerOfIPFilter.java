f1f2251064389dd973777bf3cf9dc7bcf3a94a69
zhangmingsheng <z1m6s3@163.com>
2016-11-03 17:34:57 +0800

    Update ReducerOfIPFilter.java

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -14,17 +14,22 @@ public class ReducerOfIPFilter {
 				"dd/MMM/yyyy:HH:mm:ss", locale);
 		SimpleDateFormat regularFormat = new SimpleDateFormat(
 				"yyyy-MM-dd-HH:mm:ss");
+		//hashSet作用是去重
 		HashSet<Object> hs = new HashSet<Object>();
 		Scanner scanner = new Scanner(System.in);
 		String[] str = null;
+		//第一个参数是beginTime
 		Date beginDate = regularFormat.parse(args[0]);
+		////第一个参数是endTime
 		Date endDate = regularFormat.parse(args[1]);
 		while (scanner.hasNext()) {
 			String line = scanner.nextLine();
 			str = line.split("\t");
 			Date lineDate = inputFormat.parse(str[1]);
-			if (lineDate.compareTo(endDate) < 0) {
-				if (lineDate.compareTo(beginDate) > 0) {
+			//判断时间是否在[beginTime, endTime]内
+			if (lineDate.compareTo(endDate) <= 0) {
+				if (lineDate.compareTo(beginDate) >= 0) {
+					//在里面，添加到hs里
 					hs.add(str[0]);
 				}
 			}
